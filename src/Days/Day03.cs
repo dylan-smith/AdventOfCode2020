29cc1925c682b113231fe7505be65ce69cc92d91
Dylan Smith <dylan-smith@github.com>
2020-12-02 23:15:02 -0600

    Day 3 solved

--- src/Days/Day03.cs
+++ src/Days/Day03.cs
@@ -1,20 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace AdventOfCode.Days
+﻿namespace AdventOfCode.Days
 {
     [Day(2020, 3)]
     public class Day03 : BaseDay
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var grid = input.CreateCharGrid();
+
+            return CountTrees(grid, 3, 1).ToString();
+        }
+
+        private long CountTrees(char[,] grid, int right, int down)
+        {
+            var x = 0;
+            var y = 0;
+            var count = 0L;
+
+            while (y < grid.GetUpperBound(1))
+            {
+                x += right;
+                y += down;
+
+                x %= grid.GetUpperBound(0) + 1;
+
+                if (grid[x, y] == '#')
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            var grid = input.CreateCharGrid();
+
+            var a = CountTrees(grid, 1, 1);
+            var b = CountTrees(grid, 3, 1);
+            var c = CountTrees(grid, 5, 1);
+            var d = CountTrees(grid, 7, 1);
+            var e = CountTrees(grid, 1, 2);
+
+            return (a * b * c * d * e).ToString();
         }
     }
 }
