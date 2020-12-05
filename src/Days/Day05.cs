282bc6038b85faee4f74dc8cc45016ba195d07ed
Dylan Smith <dylan-smith@github.com>
2020-12-04 23:18:56 -0600

    Day 5 solved - not refactored yet

--- src/Days/Day05.cs
+++ src/Days/Day05.cs
@@ -9,12 +9,69 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            return input.Lines().Max(x => CalcSeatId(x)).ToString();
+        }
+
+        private int CalcSeatId(string input)
+        {
+            return GetRow(input.Substring(0, 7)) * 8 + GetColumn(input.Substring(7));
+        }
+
+        private int GetRow(string input)
+        {
+            var min = 0;
+            var max = 127;
+
+            foreach (var c in input)
+            {
+                if (c == 'F')
+                {
+                    max -= (max - min + 1) / 2;
+                }
+
+                if (c == 'B')
+                {
+                    min += (max - min + 1) / 2;
+                }
+            }
+
+            return min;
+        }
+
+        private int GetColumn(string input)
+        {
+            var min = 0;
+            var max = 8;
+
+            foreach (var c in input)
+            {
+                if (c == 'L')
+                {
+                    max -= (max - min + 1) / 2;
+                }
+
+                if (c == 'R')
+                {
+                    min += (max - min + 1) / 2;
+                }
+            }
+
+            return min;
         }
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            var seats = input.Lines().Select(CalcSeatId).OrderBy(x => x).ToList();
+
+            for (var i = 0; i < seats.Count; i++)
+            {
+                if (seats[i] != seats[0] + i)
+                {
+                    return (seats[0] + i).ToString();
+                }
+            }
+
+            throw new Exception();
         }
     }
 }
