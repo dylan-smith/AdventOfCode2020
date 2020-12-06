703185524b78893e24965a9734dfb75b03d0397d
Dylan Smith <dylan-smith@github.com>
2020-12-06 13:31:54 -0600

    Day 6 solved

--- src/Days/Day06.cs
+++ src/Days/Day06.cs
@@ -9,12 +9,60 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var groups = input.Paragraphs().Select(p => CountYesAnswers(p)).Sum();
+
+            return groups.ToString();
+        }
+
+        private int CountYesAnswers(string group)
+        {
+            var questions = new HashSet<char>();
+            foreach (var c in group)
+            {
+                if (c != '\n' && c != '\r')
+                {
+                    questions.Add(c);
+                }
+            }
+
+            return questions.Count();
+        }
+
+        private int CountYesAnswers2(string group)
+        {
+            var questions = new HashSet<char>();
+
+            foreach (var c in group)
+            {
+                if (c != '\n' && c != '\r')
+                {
+                    questions.Add(c);
+                }
+            }
+
+            foreach (var line in group.Lines())
+            {
+                var goodQuestions = new HashSet<char>();
+
+                foreach (var q in questions)
+                {
+                    if (line.Contains(q))
+                    {
+                        goodQuestions.Add(q);
+                    }
+                }
+
+                questions = goodQuestions;
+            }
+
+            return questions.Count();
         }
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            var groups = input.Paragraphs().Select(p => CountYesAnswers2(p)).Sum();
+
+            return groups.ToString();
         }
     }
 }
