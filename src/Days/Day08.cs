8fe705d2e7975ef61b4775872c6de0fd58c64e8a
Dylan Smith <dylan-smith@github.com>
2020-12-07 23:18:49 -0600

    Day 8 solved

--- src/Days/Day08.cs
+++ src/Days/Day08.cs
@@ -9,12 +9,119 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var vm = new BootCodeVM(input);
+            var result = vm.Run();
+
+            return result.ToString();
         }
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            var instructions = input.ParseLines(BootCodeVM.ParseInstruction).ToList();
+
+            for (var i = 0; i < instructions.Count; i++)
+            {
+                if (instructions[i].instruction == "nop")
+                {
+                    instructions[i] = ("jmp", instructions[i].arg);
+
+                    var vm = new BootCodeVM(instructions);
+                    var result = vm.Run2();
+
+                    if (result.result)
+                    {
+                        return result.acc.ToString();
+                    }
+
+                    instructions[i] = ("nop", instructions[i].arg);
+                }
+                else if (instructions[i].instruction == "jmp")
+                {
+                    instructions[i] = ("nop", instructions[i].arg);
+
+                    var vm = new BootCodeVM(instructions);
+                    var result = vm.Run2();
+
+                    if (result.result)
+                    {
+                        return result.acc.ToString();
+                    }
+
+                    instructions[i] = ("jmp", instructions[i].arg);
+                }
+            }
+
+            return "failed";
+        }
+    }
+
+    public class BootCodeVM
+    {
+        private long _accumulator;
+        private int _ip = 0;
+        public List<(string instruction, int arg)> _instructions;
+
+        public BootCodeVM(string program)
+        {
+            _instructions = program.ParseLines(ParseInstruction).ToList();
+        }
+
+        public BootCodeVM(List<(string instruction, int arg)> program)
+        {
+            _instructions = program;
+        }
+
+        public static (string instruction, int arg) ParseInstruction(string arg)
+        {
+            return (arg.Words().First(), int.Parse(arg.Words().Last()));
+        }
+
+        public long Run()
+        {
+            var seen = new List<int>();
+
+            while (!seen.Contains(_ip))
+            {
+                seen.Add(_ip);
+                var instruction = _instructions[_ip];
+                _ip++;
+                ExecuteInstruction(instruction);
+            }
+
+            return _accumulator;
+        }
+
+        public (long acc, bool result) Run2()
+        {
+            var seen = new List<int>();
+
+            while (!seen.Contains(_ip))
+            {
+                seen.Add(_ip);
+                var instruction = _instructions[_ip];
+                _ip++;
+                ExecuteInstruction(instruction);
+
+                if (_ip == _instructions.Count)
+                {
+                    return (_accumulator, true);
+                }
+            }
+
+            return (_accumulator, false);
+        }
+
+        private void ExecuteInstruction((string instruction, int arg) instruction)
+        {
+            if (instruction.instruction == "acc")
+            {
+                _accumulator += instruction.arg;
+            }
+
+            if (instruction.instruction == "jmp")
+            {
+                _ip += instruction.arg - 1;
+            }
         }
     }
 }
