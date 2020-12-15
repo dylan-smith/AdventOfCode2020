6e089a823992ec77afb641036ae9a62683a1eecc
Dylan Smith <dylan-smith@github.com>
2020-12-15 14:58:15 -0600

    Day 15 solved with tests

--- src/Days/Day15.cs
+++ src/Days/Day15.cs
@@ -9,12 +9,45 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var numbers = input.Integers().ToList();
+
+            return MemoryGame(numbers, 2020).ToString();
+        }
+
+        public int MemoryGame(IEnumerable<int> starting, int position)
+        {
+            var lastSpoken = new Dictionary<int, int>();
+
+            for (var i = 0; i < starting.Count() - 1; i++)
+            {
+                lastSpoken.SafeSet(starting.ElementAt(i), i);
+            }
+
+            var target = starting.Last();
+
+            for (var pos = starting.Count(); pos < position; pos++)
+            {
+                if (lastSpoken.ContainsKey(target))
+                {
+                    var age = pos - lastSpoken[target] - 1;
+                    lastSpoken[target] = pos - 1;
+                    target = age;
+                }
+                else
+                {
+                    lastSpoken[target] = pos - 1;
+                    target = 0;
+                }
+            }
+
+            return target;
         }
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            var numbers = input.Integers().ToList();
+            
+            return MemoryGame(numbers, 30000000).ToString();
         }
     }
 }
