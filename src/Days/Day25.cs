ae95f48a5c4f7f9a1e7bb9997e5771014c4d96fb
Dylan Smith <dylan-smith@github.com>
2020-12-29 17:19:01 -0600

    Day 25

--- src/Days/Day25.cs
+++ src/Days/Day25.cs
@@ -9,7 +9,30 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var doorPublicKey = input.Integers().First();
+            var cardPublicKey = input.Integers().Last();
+
+            var value = 1L;
+            var subject = 7;
+            var doorLoop = 0;
+
+            while (value != doorPublicKey)
+            {
+                value *= subject;
+                value %= 20201227;
+                doorLoop++;
+            }
+
+            subject = cardPublicKey;
+            value = 1L;
+
+            for (var i = 0; i < doorLoop; i++)
+            {
+                value *= subject;
+                value %= 20201227;
+            }
+
+            return value.ToString();
         }
 
         public override string PartTwo(string input)
